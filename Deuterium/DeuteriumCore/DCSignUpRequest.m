9cc1b589fe18d2b65e5383b082c099a288146f66
xcvista <xcvista@me.com>
2013-05-22 17:55:41 +0800

    4.1.18: Skeleton Sign Up

---
+++ Deuterium/DeuteriumCore/DCSignUpRequest.m
@@ -0,0 +1,13 @@
+//
+//  DCSignUpRequest.m
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-22.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import "DCSignUpRequest.h"
+
+@implementation DCSignUpRequest
+
+@end
