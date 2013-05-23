9b66ed107a83e76a7e4a1d66a3f614cda98cf2cb
xcvista <xcvista@me.com>
2013-05-23 13:36:50 +0800

    4.1.23: Skeleton MobileDeuterium

---
+++ Deuterium/MobileDeuterium/DCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCAppDelegate.h
+//  MobileDeuterium
+//
+//  Created by Maxthon Chan on 13-5-23.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Deuterium/MobileDeuterium/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileDeuterium
+//
+//  Created by Maxthon Chan on 13-5-23.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCAppDelegate class]));
+    }
+}
