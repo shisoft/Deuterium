3b1b88af754e94ee3ba3aabf3e68dfe262903041
xcvista <xcvista@me.com>
2013-05-24 23:37:52 +0800

    4.1.29: Empty template for DCUniversalContact

---
+++ Deuterium/DeuteriumCore/DCUniversalContact.h
@@ -0,0 +1,13 @@
+//
+//  DCUniversalContact.h
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-24.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import <CGIJSONObject/CGIJSONObject.h>
+
+@interface DCUniversalContact : CGIPersistantObject
+
+@end
---
+++ Deuterium/DeuteriumCore/DCUniversalContact.m
@@ -0,0 +1,13 @@
+//
+//  DCUniversalContact.m
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-24.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import "DCUniversalContact.h"
+
+@implementation DCUniversalContact
+
+@end
