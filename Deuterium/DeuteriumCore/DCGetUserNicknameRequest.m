2b8b14d30c6b70f0fcd543e8571fb00eab1d8fdc
shisoft <shisoftgenius@hotmail.com>
2013-05-25 12:00:53 +0800

    Merge branch 'master' of https://github.com/xcvista/Deuterium
    Conflicts:
    	Deuterium/Deuterium.xcodeproj/project.pbxproj

---
+++ Deuterium/DeuteriumCore/DCGetUserNicknameRequest.m
@@ -0,0 +1,13 @@
+//
+//  DCGetUserNicknameRequest.m
+//  Deuterium
+//
+//  Created by John Shi on 13-5-25.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import "DCGetUserNicknameRequest.h"
+
+@implementation DCGetUserNicknameRequest
+
+@end
---
+++ Deuterium/MobileDeuterium/DCPreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCPreferencesViewController.h
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-25.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCPreferencesViewController : UITableViewController
+
+@end
