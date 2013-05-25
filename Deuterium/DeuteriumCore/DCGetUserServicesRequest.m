12928b34007811ab92558f5e4bef3b21d494fa3e
shisoft <shisoftgenius@hotmail.com>
2013-05-25 11:29:36 +0800

    4.1.44 Add GetUserServices interface

---
+++ Deuterium/DeuteriumCore/DCGetUserServicesRequest.m
@@ -0,0 +1,13 @@
+//
+//  DCGetUserServicesRequest.m
+//  Deuterium
+//
+//  Created by John Shi on 13-5-25.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import "DCGetUserServicesRequest.h"
+
+@implementation DCGetUserServicesRequest
+
+@end
