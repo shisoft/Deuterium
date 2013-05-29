0d62fb61681660f3034bcf46e34fcea2135f8bf8
xcvista <xcvista@me.com>
2013-05-29 20:05:00 +0800

    4.1.102: Skeleton new image

---
+++ Deuterium/MobileDeuterium/DCCreateNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCCreateNewsViewController.h
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-29.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCCreateNewsViewController : UIViewController
+
+@end
---
+++ Deuterium/MobileDeuterium/DCCreateNewsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DCCreateNewsViewController.m
+//  Deuterium
+//
+//  Created by Maxthon Chan on 13-5-29.
+//  Copyright (c) 2013年 muski. All rights reserved.
+//
+
+#import "DCCreateNewsViewController.h"
+
+@interface DCCreateNewsViewController ()
+
+@end
+
+@implementation DCCreateNewsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
